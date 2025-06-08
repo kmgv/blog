3c90f76af7d8f988a5c7cb48183b5dee6bcda73e
Maciej Sobczak <sobczak.maciej.ms@gmail.com>
2025-06-08 15:28:15 +0200

    Fix syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,11 @@ import umami from "@yeskunall/astro-umami";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nbit.it",
-  integrations: [sitemap(), mdx(), pagefind(), umami({ id: "72bce35c-6e17-40ce-9e80-d6224b77445e", endpointUrl "https://trk2.nbit.blog" })],
+  integrations: [
+    sitemap(),
+    mdx(),
+    pagefind(),
+    umami({ id: "72bce35c-6e17-40ce-9e80-d6224b77445e", endpointUrl: "https://trk2.nbit.blog" })],
   vite: {
     plugins: [tailwindcss()],
   },
