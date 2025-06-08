2c2ffebdd0521d6fa9277e89e2232bd314519e48
Maciej Sobczak <sobczak.maciej.ms@gmail.com>
2025-06-08 15:18:27 +0200

    Swtich umami to astro-umami so tracking works correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
+import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nbit.it",
-  integrations: [sitemap(), mdx(), pagefind()],
+  integrations: [sitemap(), mdx(), pagefind(), umami({ id: "72bce35c-6e17-40ce-9e80-d6224b77445e" })],
   vite: {
     plugins: [tailwindcss()],
   },
