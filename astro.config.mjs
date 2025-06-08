c540130516333a8b9bcdc1e735d7141bd452b451
Maciej Sobczak <sobczak.maciej.ms@gmail.com>
2025-06-08 15:55:39 +0200

    remove astro-umami, it doesn't work

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,11 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
-import umami from "@yeskunall/astro-umami";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nbit.it",
-  integrations: [
-    sitemap(),
-    mdx(),
-    pagefind(),
-    umami({ id: "72bce35c-6e17-40ce-9e80-d6224b77445e", endpointUrl: "https://trk2.nbit.blog" })],
+  integrations: [sitemap(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
   },
