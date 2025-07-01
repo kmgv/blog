35aa73925b7b3bc9d933df4cda860a439dc9449b
Maciej Sobczak <sobczak.maciej.ms@gmail.com>
2025-07-01 17:15:53 +0200

    nbit.it -> nbit.blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nbit.it",
+  site: "https://nbit.blog",
   integrations: [sitemap(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
