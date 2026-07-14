1b126da2efccde2dcf3858987bafcc9e03d07158
Maciej Sobczak <sobczak.maciej.ms@gmail.com>
2026-07-14 18:05:11 +0200

    Revert "change blog title"
    This reverts commit 838395765a3e1733c3bfad3e8c0bb6bf1980721e.

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Nth bit",
+  TITLE: "No bullshit IT",
   DESCRIPTION: "Personal blog with a focus just a meat-and-potatoes approach.",
   EMAIL: "sobczak.maciej.ms@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
